72b1d1d27ae5334583472c1974910fc7993fa153
Kevin Brewster <brewsterdesigns@gmail.com>
2019-10-09 18:26:01 -0400

    made public

--- Sources/SwiftLinuxBLE/Peripheral.swift
+++ Sources/SwiftLinuxBLE/Peripheral.swift
@@ -25,40 +25,35 @@ extension Peripheral {
             try peripheral.controller.iBeacon(beacon, flags: flags, interval: .min, timeout: .default)
         }
     }
-    public func add(service: Service) {
-        do {
-            // Find all the characteristics for the service
-            let characteristics = Mirror(reflecting: service).children.compactMap {
-                $0.value as? CharacteristicType
-            }
-            
-            let gattCharacteristics = characteristics.map {
-                GATT.Characteristic(uuid: $0.uuid, value: $0.data, permissions: $0.permissions, properties: $0.properties, descriptors: $0.descriptors)
-            }
-            
-            let gattService = GATT.Service(uuid: service.uuid, primary: true, characteristics: gattCharacteristics)
-            let _ = try peripheral.add(service: gattService)
-            
-            
-            for var characteristic in characteristics {
-                guard let handle = peripheral.characteristics(for: characteristic.uuid).last else { continue }
-                
-                print("Characteristic \(characteristic.uuid) with permissions \(characteristic.permissions) and \(characteristic.descriptors.count) descriptors")
-                
-                // Register as observer for each characteristic
-                characteristic.didSet = { [weak self] in
-                    NSLog("MyPeripheral: characteristic \(characteristic.uuid) did change with new value \($0)")
-                    self?.peripheral[characteristic: handle] = $0
-                }
-              
-                characteristicsByHandle[handle] = characteristic
-                
-            }
-            services += [service]
-            
-        } catch let error {
-            print("Error adding service: \(error)")
+    public func add(service: Service) throws {
+        // Find all the characteristics for the service
+        let characteristics = Mirror(reflecting: service).children.compactMap {
+            $0.value as? CharacteristicType
         }
+        
+        let gattCharacteristics = characteristics.map {
+            GATT.Characteristic(uuid: $0.uuid, value: $0.data, permissions: $0.permissions, properties: $0.properties, descriptors: $0.descriptors)
+        }
+        
+        let gattService = GATT.Service(uuid: service.uuid, primary: true, characteristics: gattCharacteristics)
+        let _ = try peripheral.add(service: gattService)
+        
+        
+        for var characteristic in characteristics {
+            guard let handle = peripheral.characteristics(for: characteristic.uuid).last else { continue }
+            
+            print("Characteristic \(characteristic.uuid) with permissions \(characteristic.permissions) and \(characteristic.descriptors.count) descriptors")
+            
+            // Register as observer for each characteristic
+            characteristic.didSet = { [weak self] in
+                NSLog("MyPeripheral: characteristic \(characteristic.uuid) did change with new value \($0)")
+                self?.peripheral[characteristic: handle] = $0
+            }
+          
+            characteristicsByHandle[handle] = characteristic
+            
+        }
+        services += [service]      
     }
     
     public func didWrite(_ confirmation: GATTWriteConfirmation<Central>) {
